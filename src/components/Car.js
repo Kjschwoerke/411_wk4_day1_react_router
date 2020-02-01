4ec198723f1460225c5c11e6b891e7a52c66cf3b
kjschwoerke <kjschwoerke@yahoo.com>
2020-01-31 20:17:54 -0600

    initial push

--- src/components/Car.js
+++ src/components/Car.js
@@ -4,9 +4,22 @@ import cars from '../cars.json'
 // Container, Paper, Chip //
 
 const Car = (props) => {
+    let id = props.match.params.id
+    console.log(JSON.stringify(cars[id - 1]) + "this is the id")
+    let car = cars[id - 1]
+    console.log(car.id , car.Name, car.Cylinders)
     return (
-        <h1>A specific car</h1>
+    <>
+    <h1>{car.Name}</h1>
+    <p>Cylinders: {car.Cylinders}</p>
+    <p>Displacement: {car.Displacement}</p>
+    <p>Miles Per Gallon: {car.Miles_per_Gallon}</p>
+    <p>Horsepower: {car.Horsepower}</p>
+    <p>Model Year: {car.Year}</p>
+    <p>Origin: {car.Origin}</p>
+    </>
     )
+    
 }
 
 export default Car
